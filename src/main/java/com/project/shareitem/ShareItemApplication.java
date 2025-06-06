05a16ba786a41cc36aa888b78bf3f862e526537c
Stas <stastheone@proton.me>
2025-06-06 13:25:03 +0300

    Initial commit

---
+++ src/main/java/com/project/shareitem/ShareItemApplication.java
@@ -0,0 +1,13 @@
+package com.project.shareitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItemApplication.class, args);
+    }
+
+}
